40e8f67a5c2ed730829abaa474423833eaeeb299
blairfix <blairfix@fastmail.com>
2025-05-14 06:15:48 -0600

    first commit

---
+++ configuration.nix
@@ -0,0 +1,73 @@
+{ config, pkgs, ... }:
+{
+    imports =
+	[ 
+	    ./hardware-configuration.nix
+
+	    # configs
+	    ./packages.nix
+	    ./nvim.nix
+
+	];
+
+    # bootloader
+    boot.loader.grub = {
+	enable = true;
+	useOSProber = true;
+	device = "/dev/sda";
+    };
+
+
+    # kernel
+    boot.kernelPackages = pkgs.linuxPackages_latest;
+
+    # hostname
+    networking.hostName = "nas"; 
+
+    # networking
+    networking.networkmanager.enable = true;
+
+    # time zone
+    time.timeZone = "America/Edmonton";
+
+    # utf8 setting
+    i18n.defaultLocale = "en_CA.UTF-8";
+
+    # tailscale
+    services.tailscale.enable = true;
+
+    # ssh
+    services.openssh = {
+	enable = true;
+	ports = [ 22 ];
+	settings = {
+	    PasswordAuthentication = false;
+	    PermitRootLogin = "no"; 
+	};
+    };
+    
+    # docker
+    virtualisation.docker.enable = true;
+
+    # user account. Add password with ‘passwd’.
+    users.users.blair = {
+	isNormalUser = true;
+	description = "blair";
+	extraGroups = [ 
+	    "networkmanager" 
+	    "wheel" 
+	    "docker"
+	];
+    };
+
+    # unfree packages
+    nixpkgs.config.allowUnfree = true;
+
+
+    # for bash scripts 
+    services.envfs.enable = true;
+
+    # version 
+    system.stateVersion = "24.05"; 
+
+}
