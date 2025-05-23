9bee03123c5522a79436f475d45392c795f1962c
blairfix <blairfix@fastmail.com>
2025-05-23 09:54:06 -0600

    update timer

---
+++ timers/update.nix
@@ -0,0 +1,29 @@
+{ config, pkgs, ... }:
+{
+
+    # start update
+    #----------------------------------------
+
+    systemd.timers."update" = {
+	wantedBy = [ "timers.target" ];
+	timerConfig = {
+	    OnCalendar = "*-*-02 12:00:00";
+	    Persistent = "true";
+	    Unit = "update.service";
+	};
+    };
+
+    systemd.services."update" = {
+	serviceConfig = {
+	    Type = "simple";
+	    User = "root";
+	};
+
+	path = with pkgs; [ 
+	    bash
+	];
+	script = ''
+	    bash /home/blair/bin/update.sh
+	    '';
+    };
+}
