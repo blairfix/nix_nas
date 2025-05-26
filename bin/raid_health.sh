051256e2f281db236e0f1bb1ba3bcc98af40dc4e
blairfix <blairfix@fastmail.com>
2025-05-26 13:04:24 -0600

    send script

--- bin/raid_health.sh
+++ bin/raid_health.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# get raid health
 sudo mdadm --detail /dev/md0 > /home/blair/raid_health.txt
 
 
+# send to blair server
+scp -p 1497 -i /home/blair/.ssh/blair_server  /home/blair/raid_health.txt blair@100.82.137.69:/home/blair/Projects/raid_status/
