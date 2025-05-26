df6969ed94800b311ca0ef14a966e999c01b5b65
blairfix <blairfix@fastmail.com>
2025-05-26 13:06:08 -0600

    typo

--- bin/raid_health.sh
+++ bin/raid_health.sh
@@ -5,4 +5,4 @@ sudo mdadm --detail /dev/md0 > /home/blair/raid_health.txt
 
 
 # send to blair server
-scp -p 1497 -i /home/blair/.ssh/blair_server  /home/blair/raid_health.txt blair@100.82.137.69:/home/blair/Projects/raid_status/
+scp -P 1497 -i /home/blair/.ssh/blair_server  /home/blair/raid_health.txt blair@100.82.137.69:/home/blair/Projects/raid_status/
