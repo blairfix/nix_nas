6d5c55fbb4a16b3bed4c8e30688edecba44352c0
blairfix <blairfix@fastmail.com>
2026-06-07 08:59:18 -0600

    borg list update

--- configuration.nix
+++ configuration.nix
@@ -48,13 +48,6 @@
     services.tailscale.enable = true;
 
     # disable sleep
-#    systemd.sleep.settings.Sleep = ''
-#	AllowSuspend=no
-#	AllowHibernation=no
-#	AllowHybridSleep=no
-#	AllowSuspendThenHibernate=no
-#	'';
-
     systemd.sleep.settings = {
 	Sleep = {
 	    AllowSuspend = "no";
